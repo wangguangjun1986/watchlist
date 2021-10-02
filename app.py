18a19a86b2d1b4b16ab2a01858b7a266f2ba6154
junjun <wangguangjun1986@163.com>
2021-10-02 23:50:37 +0800

    third chapter

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask,escape,url_for
+from flask import Flask,escape,url_for,render_template
 
 app=Flask(__name__)
 
+name = 'wangguangjun'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
-def hello():
-    return 'welcome to my watchlist!'
+def index():
+    return render_template('index.html',name=name,movies=movies)
     
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % escape(name)
