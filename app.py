bbe11b188a0e60715f961766bf99db9fc88b309f
junjun <wangguangjun1986@163.com>
2021-10-03 01:15:49 +0800

    fifth chapter

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask,escape,url_for,render_template
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import click
+
 
 app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:w@127.0.0.1:3306/watch'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db=SQLAlchemy(app)
+
 
 name = 'wangguangjun'
 movies = [
@@ -16,7 +25,58 @@ movies = [
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
 
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'wangguangjun'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+  
+    
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(20))
+    
+class Movie(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(60))
+    year=db.Column(db.String(4))
+    
 @app.route('/')
 def index():
+    name=User.query.first()
+    movies=Movies.query.all()
     return render_template('index.html',name=name,movies=movies)
     
